8cda9064493ffc749398bea92b4e89d87f248e4b
Jordan Hewus <jebuspwnsis@gmail.com>
2015-06-02 13:23:30 -0700

    make it better

---
+++ server.py
@@ -0,0 +1,29 @@
+
+from flask import Flask, render_template, request
+import os
+import requests
+
+app = Flask(__name__)
+#app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
+janrain_engage_root_url = "https://jordantestapp.rpxnow.com//api/v2/auth_info"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/my_url', methods=['POST'])
+def my_url():
+    token = request.form['token']
+    engage_api_params = {
+        'apiKey': os.environ['JANRAIN_ENGAGE_API_KEY'],
+        'token': token
+    }
+    user_data = requests.get(janrain_engage_root_url, params=engage_api_params)
+    auth_info = user_data.json()
+    profile = auth_info['profile']
+    name = profile['name']['formatted']
+    #store_user_data_in_postgres(name)
+    return render_template('logged_in.html', name=name)
+
+app.run(debug=True, port=3000)
